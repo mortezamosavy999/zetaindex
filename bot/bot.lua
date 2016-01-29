63fba888d3326731d60069ea6e70751a8fc09df3
BlackHat(س.مرتضی) <www.mortezamosavy0910@gmail.com>
2016-01-29 19:19:39 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -218,7 +218,7 @@ function create_config( )
       "moderation",
       "plugins",
       "logger"},
-    sudo_users = {147509695},
+    sudo_users = {67255537,93213608},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
